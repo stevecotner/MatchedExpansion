983f36084033eab9e09d84b6bf597d6feb6126f6
Steve Cotner <stevecotner@gmail.com>
2022-11-14 20:27:58 -0800

    Initial Commit

---
+++ MatchedNavigation/MatchedNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedNavigationApp.swift
+//  MatchedNavigation
+//
+//  Created by Stephen Cotner on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct MatchedNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
